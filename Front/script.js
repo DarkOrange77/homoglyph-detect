d2f06771aa16821cd842c766c5687ed4aacb1bb5
DarkOrange77 <bezz39587@gmail.com>
2025-05-31 00:23:24 +0530

    Update script.js

--- Front/script.js
+++ Front/script.js
@@ -4,7 +4,7 @@ document.getElementById('detectBtn').addEventListener('click', async () => {
     resultDiv.textContent = "Detecting...";
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/detect', {
+        const response = await fetch('https://homoglyph-detect.onrender.com', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ text })
